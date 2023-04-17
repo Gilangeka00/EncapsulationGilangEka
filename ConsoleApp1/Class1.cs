788962e1ddb2a307dd48caa60895b7c58a5f45bb
gilang <gilangekayanda@students.amikom.ac.id>
2023-04-17 21:43:02 +0700

    first commit

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Encapsuletion;
+
+namespace Encapsuletion
+{
+    internal class laptop
+    {
+        //field
+        public string MEREK;
+        public string MODEL;
+        protected string TAHUN;
+
+        //propeties
+        public string merek
+        {
+            get { return MEREK; }
+            set { MEREK = value; }
+        }
+        public string model
+        {
+            get { return MODEL; }
+            set { MODEL = value; }
+        }
+
+        public string tahun
+        {
+            get { return TAHUN; }
+            set { TAHUN = value; }
+        }
+        //method
+        public void SetLaptop(string merek, string model)
+        {
+            this.MEREK = merek;
+            this.MODEL = model;
+            Console.WriteLine("Merek Laptop : {0}", this.MEREK);
+            Console.WriteLine("Model Laptop : {0}", this.MODEL);
+           
+        }
+        public void SetTahunkeluar(string tahun)
+        {
+            this.TAHUN = tahun;
+            Console.WriteLine("Tahun Keluaran : {0}", this.TAHUN);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Encapsuletion;
+
+namespace Encapsuletion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            laptop lp1 = new laptop();
+            lp1.merek = "ACER";
+            lp1.model = "NITRO 5";
+            lp1.tahun = "2023";
+
+            Console.WriteLine($"Merek Laptop : {lp1.merek}" + "\n" + $"Model Laptop : {lp1.model}" + "\n" + $"Tahun Keluaran : {lp1.tahun}");
+            Console.WriteLine("----------------------");
+
+            laptop lp2 = new laptop();
+            lp2.SetLaptop("LENOVO", "LEGION 5");
+            lp2.SetTahunkeluar("2023");
+            Console.Write(lp2);
+
+            Console.ReadKey();
+        }
+    }
+}
